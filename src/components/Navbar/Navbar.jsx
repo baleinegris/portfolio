79d2c0595b0fdc42597ff3e7b1001fdd20c11832
Oscar Heath <136947514+baleinegris@users.noreply.github.com>
2024-07-31 20:55:44 -0400

    built navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png";
+import '../../index.css';
+
+function Navbar() {
+    const [active, setActive] = useState('Home');
+
+    return (
+        <nav className="w-full flex items-center py-5 fixed top-0 z-20 bg-primary">
+            <div className="w-full flex justify-left items-center mx-auto">
+            <Link 
+                to='/'
+                className="flex items-center gap-2"
+                onClick={() => {
+                    setActive('Home');;
+                    window.scrollTo(0, 0);
+                }}
+            >
+                <img src={logo} alt="logo" className="h-40 w-40 object-contain" />
+                <p className="text-white font-bold text-[18px] cursor-pointer">Oscar Heath | Baleinegris</p>
+            </Link>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
